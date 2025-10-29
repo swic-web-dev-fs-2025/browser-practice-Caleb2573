13632889db2e284aba7d45ee69d9aa932f617f5f
Caleb Yates <caleb.yates@student.swic.edu>
2025-10-28 22:58:27 -0500

    Add reset button to reset button clicks

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import "./style.css";
 
 let clickCount = 0;
 
-const greetingElement = document.getElementById("greeting");
-const button = document.getElementById("change-greeting");
-const countElement = document.getElementById("click-count");
+const greetingElement = document.getElementById(`greeting`);
+const button = document.getElementById(`change-greeting`);
+const countElement = document.getElementById(`click-count`);
 
 function updateGreeting() {
   clickCount++;
   greetingElement.textContent = `Hello, Browser JavaScript!`;
   countElement.textContent = `Button clicks: ${clickCount}`;
+  if (clickCount === 5) {
+    greetingElement.style.color = "blue";
+  }
 }
 
 button.addEventListener("click", updateGreeting);
