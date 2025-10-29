dea04418040891304cc2f36080f5ad31d36684d1
Caleb Yates <caleb.yates@student.swic.edu>
2025-10-29 00:35:14 -0500

    Remove all previous code and create personal counter app

--- src/main.js
+++ src/main.js
@@ -2,28 +2,31 @@ import "./style.css";
 
 let clickCount = 0;
 
-const greetingElement = document.getElementById(`greeting`);
-const button = document.getElementById(`change-greeting`);
 const resetButton = document.getElementById(`reset`);
 const countElement = document.getElementById(`click-count`);
-const userInput = prompt("Enter your greeting");
+const decrementButton = document.getElementById(`decrement`);
+const incrementButton = document.getElementById(`increment`);
 
-function resetGreeting() {
+function reset() {
   clickCount = 0;
   countElement.textContent = `Button clicks: ${clickCount}`;
 }
 
-function updateGreeting() {
+function increment() {
   clickCount++;
-  greetingElement.textContent = `Hello, Browser JavaScript!`;
   countElement.textContent = `Button clicks: ${clickCount}`;
-  if (clickCount >= 5) {
-    greetingElement.style.color = "blue";
+  if (clickCount >= 10) {
+    countElement.style.backgroundColor = "blue";
   }
 }
+function decrement() {
+  clickCount--;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+}
 
-resetButton.addEventListener("click", resetGreeting);
-button.addEventListener("click", updateGreeting);
+resetButton.addEventListener("click", reset);
+incrementButton.addEventListener("click", increment);
+decrementButton.addEventListener("click", decrement);
 
 document.querySelector("#app").innerHTML = `
   <h1 class="text-3xl font-bold underline">
