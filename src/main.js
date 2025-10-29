6a4a7872d60f1867228bd80ab8f6c9e559e5cb88
Caleb Yates <caleb.yates@student.swic.edu>
2025-10-28 22:11:43 -0500

    Update main.js to reflect lesson example

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import "./style.css";
 
+let clickCount = 0;
+
+const greetingElement = document.getElementById("greeting");
+const button = document.getElementById("change-greeting");
+const countElement = document.getElementById("click-count");
+
+function updateGreeting() {
+  clickCount++;
+  greetingElement.textContent = `Hello, Browser JavaScript!`;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+}
+
+button.addEventListener("click", updateGreeting);
+
 document.querySelector("#app").innerHTML = `
   <h1 class="text-3xl font-bold underline">
     Hello Vite!
   </h1>
+
 `;
