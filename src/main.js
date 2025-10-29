d1de6a13bd3f8d162747c5b9409c5ba6366bd199
Caleb Yates <caleb.yates@student.swic.edu>
2025-10-28 23:01:53 -0500

    Update updateGreeting function to change heading color after 5 clicks

--- src/main.js
+++ src/main.js
@@ -4,17 +4,23 @@ let clickCount = 0;
 
 const greetingElement = document.getElementById(`greeting`);
 const button = document.getElementById(`change-greeting`);
+const resetButton = document.getElementById(`reset`);
 const countElement = document.getElementById(`click-count`);
+
+function resetGreeting() {
+  clickCount = 0;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+}
 
 function updateGreeting() {
   clickCount++;
   greetingElement.textContent = `Hello, Browser JavaScript!`;
   countElement.textContent = `Button clicks: ${clickCount}`;
-  if (clickCount === 5) {
+  if (clickCount >= 5) {
     greetingElement.style.color = "blue";
   }
 }
-
+resetButton.addEventListener("click", resetGreeting);
 button.addEventListener("click", updateGreeting);
 
 document.querySelector("#app").innerHTML = `
